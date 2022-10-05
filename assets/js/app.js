c462f87157b8c7533c77c23bda270268c417f996
andrey <rodomir3119841985@gmail.com>
2022-10-05 10:55:42 +0300

    stoies

--- assets/js/app.js
+++ assets/js/app.js
@@ -131,3 +131,103 @@ eval("__webpack_require__(/*! E:\\программирование\\blog\\вер
 /***/ })
 
 /******/ });
+
+// const modalBtn = document.querySelectorAll('[data-modal]');
+// const body = document.body;
+// const modalClose = document.querySelectorAll('.modal__close');
+// const modal = document.querySelectorAll('.modal');
+// modalBtn.forEach(item => {
+//     item.addEventListener('click', event => {
+//         let $this = event.currentTarget;
+//         let modalAt = $this.getAttribute('data-modal');
+    
+//         let modalId = document.getElementById(modalAt);
+//         let modalContent = modalId.querySelector('.modal__content');
+        
+//         modalContent.addEventListener('click', event => {
+//             event.stopPropagation();
+//         });
+        
+//         modal.classList.add('show');
+//         body.classList.add('no-scroll');
+        
+//         setTimeout(() => {
+//             modalContent.style.transform = 'none';
+//             modalContent.style.opacity = '1';
+//         }, 1);
+        
+//     });
+// });
+
+
+// modalClose.forEach(item => {
+//     item.addEventListener('click', event => {
+//         let currentModal = event.currentTarget.closest('.modal');
+        
+//         closeModal(currentModal);
+//     });
+// });
+
+
+// modal.forEach(item => {
+//     item.addEventListener('click', event => {
+//         let currentModal = event.currentTarget;
+        
+//         closeModal(currentModal);
+//     });
+// });
+
+
+// function closeModal(currentModal) {
+//     let modalContent = currentModal.querySelector('.modal__content');
+//     modalContent.removeAttribute('style');
+    
+//     setTimeout(() => {
+//         currentModal.classList.remove('show');
+//         body.classList.remove('no-scroll');
+//     }, 200);
+// }
+
+
+const modalBtn = document.querySelectorAll('[data-modal]');//1
+const modal = document.querySelectorAll('.modal');
+const body = document.body;
+console.log(body);
+
+modalBtn.forEach(item=>{//2
+    item.addEventListener('click',(event)=>{
+        const $this = event.currentTarget;
+        let getAtribute = $this.getAttribute('data-modal');
+        let modalAt = document.getElementById(getAtribute);
+        console.log(modalAt);
+        modalAt.classList.add('show');
+        body.classList.add('no-scroll');
+
+        let modalContent = modalAt.querySelector('.modal__content');
+        modalContent.addEventListener('click',(event)=>{
+         event.stopPropagation();
+        });
+
+        setTimeout(()=>{
+         modalContent.style.transform='none';
+         modalContent.style.opacity = '1';
+        },1);
+    });
+   
+});
+
+modal.forEach(item=>{
+    item.addEventListener('click',(event)=>{
+        const modlEvent = event.currentTarget;
+
+        closeModal(modlEvent);
+    })
+});
+
+function closeModal(closeModal){
+ 
+ setTimeout(()=>{
+    closeModal.classList.remove('show');
+    body.classList.remove('no-scroll');
+ });
+}
